7ef36d03b6db43675866e66d35a31f7a2cf480d9
VarunBhalla2 <100198293+VarunBhalla2@users.noreply.github.com>
2025-08-09 21:54:56 +0530

    First test of Graphics

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+const DEAL_ORDER = ['player', 'dealer', 'player', 'dealer'];
+const playerHand = document.querySelector('.player-hand');
+const dealerHand = document.querySelector('.dealer-hand');
+const dealBtn = document.getElementById('deal');
+
+const CARD_IMAGES = {
+  placeholder: 'https://via.placeholder.com/80x120?text=Card'
+};
+
+function dealCard(toHand) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.style.backgroundImage = `url(${CARD_IMAGES.placeholder})`;
+  
+  const targetHand = toHand === 'player' ? playerHand : dealerHand;
+  targetHand.appendChild(card);
+  
+  requestAnimationFrame(() => {
+    card.classList.add('dealt');
+  });
+}
+
+dealBtn.addEventListener('click', () => {
+  playerHand.innerHTML = '';
+  dealerHand.innerHTML = '';
+  
+  DEAL_ORDER.forEach((recipient, idx) => {
+    setTimeout(() => dealCard(recipient), idx * 600);
+  });
+});
 
